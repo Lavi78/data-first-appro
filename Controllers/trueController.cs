e2e0a237659e14ec17f0add883bf6f9d2184d31d
admin <admin@CHETUIWK222>
2022-05-11 18:19:03 +0530

    Add project files.

---
+++ Controllers/trueController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace data1.Controllers
+{
+    public class trueController : Controller
+    {
+        codeeEntities obj = new codeeEntities();
+        // GET: true
+        public ActionResult Index()
+        {
+            List<emp> emps = obj.emps.ToList();
+            return View(emps);
+        }
+    }
+}
